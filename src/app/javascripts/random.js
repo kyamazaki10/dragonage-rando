9d82f4c45fc5403f0224102824b08f80267ceadf
Kumiko Yamazaki <kyamazaki@gmail.com>
2015-02-10 18:54:02 -0700

    adding some basic style to the dialogue

--- src/app/javascripts/random.js
+++ src/app/javascripts/random.js
@@ -6,9 +6,11 @@ $(function() {
     $.getJSON('dialogue.json', function(data) {
         var min = 0;
         var max = data.dialogue.length - 1;
-        var random = Math.floor(Math.random() * (max - min + 1) + min);
+        var dialogue = data.dialogue[Math.floor(Math.random() * (max - min + 1) + min)];
 
-        $('.dialogue').append(data.dialogue[random]);
+        for (var i = 0; i < dialogue.length; i++) {
+            $('.dialogue').append('<p>' + dialogue[i] + '</p>');
+        }
     });
 
 });
